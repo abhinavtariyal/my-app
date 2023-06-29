bb03c627d13f54202f77f9da4b2a7980cbbfeb3e
abhinavtariyal <abhinavtariyal@gmail.com>
2023-06-29 15:24:20 +1000

    added textbox and button

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your course Goals" style={styles.textStyle} />
+        <Button title="Tap Me" />
+      </View>
+      <View>
+        <Text>List of Goals..</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textStyle: {
+    width: "80%",
+    borderColor: "#cccccc",
+    borderWidth:1,
+    padding: 10
   },
 });
